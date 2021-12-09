2c2b9b9c29ff764157f2658e54a2badb59ac8c36
Jarl <Jarlo@bk.ru>
2021-12-09 11:04:27 +0500

    Implemented basic GameManager with score

---
+++ Assets/0_Game/Scripts/Management/GameManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace _0_Game.Scripts.Management
+{
+    public class GameManager : SingletonBehaviour<GameManager>
+    {
+        public GameState State { get; set; }
+        
+        protected override void OnAwake()
+        {
+            State = new GameState();
+        }
+    }
+}
---
+++ Assets/0_Game/Scripts/Management/GameState.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _0_Game.Scripts.Management
+{
+    public class GameState
+    {
+        public static GameState Instance => GameManager.Instance.State;
+        
+        private int score;
+
+
+        public int Score
+        {
+            get => score;
+            set
+            {
+                score = value;
+                OnScoreSetEvent?.Invoke(score);
+            }
+        }
+        
+        public bool IsStarted { get; set; }
+
+        public event Action<int> OnScoreSetEvent;
+
+        public GameState()
+        {
+            
+        }
+    }
+}
