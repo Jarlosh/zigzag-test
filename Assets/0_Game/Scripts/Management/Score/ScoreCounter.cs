dcd30dcc128bae87bf8456ebc1c3802209a36a08
Jarl <Jarlo@bk.ru>
2021-12-09 11:05:17 +0500

    Added score UI

---
+++ Assets/0_Game/Scripts/Management/Score/ScoreCounter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace _0_Game.Scripts.Management.Score
+{
+    public class ScoreCounter : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text label;
+
+        private void Start()
+        {
+            GameState.Instance.OnScoreSetEvent += SetScore;
+            SetScore(GameState.Instance.Score);
+        }
+
+        private void SetScore(int score)
+        {
+            label.text = score.ToString();
+        }
+    }
+}
